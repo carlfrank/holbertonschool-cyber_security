cf2c0ba51888da36aee733d3abe46e68086b433d
Carlos <carl-frank@hotmail.com>
2024-12-04 18:35:43 +0000

    Just 2 lines of code

--- cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-length=${1:-12} # Default to 12 if no argument is provided
-echo "Password length set to: $length"  # Debugging: Print the password length
 tr -dc '[:alnum:]' < /dev/urandom | head -c "$length"; echo
-
