1b5ccdb8fb41c193606114e4514ac27f7b13b1b1
Carlos <carl-frank7@hotmail.com>
2024-12-10 11:38:19 -0400

    Print print

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant|Admin|Tech/ {gsub(":", ","); gsub(", ", ","); gsub("https,", "https:"); print}' > ${1}.csv
+whois $1 | awk '/^(Registrant|Admin|Tech)[ ]/ {gsub(/:/, "", $1); field=$1; $1=""; value=substr($0,2); gsub(/^[ \t]+|[ \t]+$/, "", value); printf "%s,%s\n", field, value}' > $1.csv
