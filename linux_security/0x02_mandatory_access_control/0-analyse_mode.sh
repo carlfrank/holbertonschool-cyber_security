922e2a448a3d25477b77caa0d163308185540402
Carlos <carl-frank7@hotmail.com>
2025-02-24 16:19:56 -0400

    fixing the code

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Check if SELinux is installed
-if ! command -v getenforce &> /dev/null; then
-    echo "SELinux is not installed on this system."
-    exit 1
-fi
-
-# Get SELinux status
-SELINUX_STATUS=$(getenforce)
-
-# Print the current SELinux mode
-echo "SELinux status: $SELINUX_STATUS"
+echo "SELinux status: $(getenforce)"
