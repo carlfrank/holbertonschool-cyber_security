aa891556218979ec7e1a340cbf5f8d3ca032fffa
Carlos <carl-frank7@hotmail.com>
2025-02-24 16:27:22 -0400

    Fixing the code

--- linux_security/0x02_mandatory_access_control/1-security_match.sh
+++ linux_security/0x02_mandatory_access_control/1-security_match.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if AppArmor is installed
-if ! command -v apparmor_status &> /dev/null; then
-    echo "AppArmor is not installed on this system."
-    exit 1
-fi
-
-# Display AppArmor status
 apparmor_status
