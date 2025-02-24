f430f89f5eb6613ef90d24ca74f597c117dac385
Carlos <carl-frank7@hotmail.com>
2025-02-24 16:36:17 -0400

    new line again

--- linux_security/0x02_mandatory_access_control/1-security_match.sh
+++ linux_security/0x02_mandatory_access_control/1-security_match.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "apparmor module is $(lsmod | grep -q "apparmor" && echo "loaded." || echo "not loaded.")"
-aa-status
+echo "apparmor module is $(lsmod | grep -q "apparmor" && echo "loaded." || echo "not loaded.")"; aa-status
