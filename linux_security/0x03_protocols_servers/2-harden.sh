e837ecabd33ee9988d3125bb920d58556f176f8b
Carlos <carl-frank7@hotmail.com>
2025-02-27 12:18:37 -0400

    +

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -xdev -type d -perm -0002 -not -path "/proc/*" -not -path "/sys/*" 2>/dev/null | tee /dev/stdout | xargs -I{} chmod o-w {}
+find / -xdev -type d -perm -0002 -not -path "/proc/*" -not -path "/sys/*" 2>/dev/null -print -exec chmod o-w {} \+
